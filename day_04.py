33bfc1022c2d001ae9874af0bce3babfac961d8f
tau31 <tau31@pm.me>
2024-01-10 16:49:52 +0100

    solved another day :yay:

--- day_04.py
+++ day_04.py
@@ -14,7 +14,6 @@
 
 import polars as pl
 import wikipedia as wiki
-import pandas
 from bs4 import BeautifulSoup
 # hints: Cancer born in the year of the rabbit
 # Cancer timeframe:  June 22 to about July 22
@@ -22,10 +21,10 @@ from bs4 import BeautifulSoup
 
 customers = pl.read_csv("usb/noahs-customers.csv")
 customers = customers.with_columns(pl.col("birthdate").str.to_date("%Y-%m-%d"))
-
+# customers = customers.select(["customerid", "birthdate", "phone"])
 # get rabbit year ranges
 rabbit_year_html = wiki.page("Rabbit (zodiac)").html().encode("UTF-8")
-table_html = BeautifulSoup(rabbit_year_html).find(
+table_html = BeautifulSoup(rabbit_year_html, features="lxml").find(
     'table', attrs={"class": "wikitable"}).find_all('tr')
 
 # https://scrapfly.io/blog/how-to-scrape-tables-with-beautifulsoup/
@@ -40,20 +39,33 @@ for i, row in enumerate(table_html):
 rabbit_year_ranges = pl.DataFrame(rows, schema=["start_date", "end_date", "not_needed"]).select(
     pl.exclude("not_needed")).with_columns(pl.all().str.to_date("%d %B %Y"))
 
-bd_ranges = customers.select(
-    pl.col("birthdate").min().alias("min_db"),
-    pl.col("birthdate").max().alias("max_bd")
-)
+# help with the join_as_of here:
+# https://discord.com/channels/908022250106667068/1014967651656814694/1179672540038299678
+rabbit_cust = customers.sort("birthdate").join_asof(
+    rabbit_year_ranges.sort("start_date"),
+    left_on="birthdate", right_on="start_date", strategy="backward").filter(
+    pl.col("birthdate").is_between("start_date", "end_date")).select(
+    pl.exclude("start_date", "end_date"))
+
+# Cancer range June 22 to about July 22
+
+possible_neighbors = rabbit_cust.filter(pl.col("birthdate").dt.month().is_in([6, 7])).with_columns(
+    pl.concat_str([pl.col("birthdate").dt.year(), pl.lit('6'), pl.lit(
+        '22')], separator="-").str.to_date("%Y-%m-%d").alias("cancer_start"),
+    pl.concat_str([pl.col("birthdate").dt.year(), pl.lit('7'), pl.lit(
+        '22')], separator="-").str.to_date("%Y-%m-%d").alias("cancer_end")
+).filter(
+    pl.col("birthdate").is_between("cancer_start", "cancer_end")
+).select(pl.exclude("cancer_start", "cancer_end"))
+
+contractor = pl.read_csv("usb/contractor.csv")
+
+contractor_zip = contractor.select(
+    pl.col("citystatezip").str.extract_all("\d+").explode()).item()
 
 
-possible_rabbit_range = rabbit_year_ranges.filter(
-    pl.col("start_date") >= bd_ranges["min_db"],
-    pl.col("end_date") <= bd_ranges["max_bd"])
+possible_neighbors = possible_neighbors.with_columns(
+    pl.col("citystatezip").str.extract_all("\d+").explode().alias("zip")
+).filter(pl.col("zip").eq(contractor_zip))
 
-possible_rabbit_range.select(pl.col("end_date")).to_struct()
-
-customers.filter(pl.col("birthdate").is_between(possible_rabbit_range.select(
-    pl.col("start_date")).to_struct(), possible_rabbit_range.select(pl.col("end_date")).to_struct()))
-
-
-rabbit_year_ranges["start_date"].to_list()
+print(f"The neighbor contact is {possible_neighbors['phone'][0]}")
